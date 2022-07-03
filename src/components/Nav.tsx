954aea13077d1dfc4483b033e318e37cdc11dc5a
giggi1226 <gpacheco003@gmail.com>
2022-07-03 16:25:42 -0400

    updated nav styles

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,8 +6,8 @@ const Nav = () => {
 
   return (
     <div className="flex flex-col bg-white bg-opacity-5 justify-center w-[200px] items-center h-screen">
-      <Link to='/' className={`flex border-l-green text-white hover:border-l-[3px] w-full justify-center ${pathname === '/' ? 'bg-black border-l-[3px]': ''}`}>Home</Link>
-      <Link to='/podcasts' className={`flex border-l-green text-white hover:border-l-[3px] w-full justify-center ${pathname === '/podcasts' ? 'bg-black border-l-[3px]': ''}`}>Podcasts</Link>
+      <Link to='/' className={`flex items-center h-[40px] border-l-green text-white hover:border-l-[3px] w-full justify-center ${pathname === '/' ? 'bg-black border-l-[3px]': ''}`}>Home</Link>
+      <Link to='/podcasts' className={`flex items-center h-[40px] border-l-green text-white hover:border-l-[3px] w-full justify-center ${pathname === '/podcasts' ? 'bg-black border-l-[3px]': ''}`}>Podcasts</Link>
     </div>
   )
 }
