7a65618e03ce4375a822c5d58e631e58de205ff6
giggi1226 <gpacheco003@gmail.com>
2023-11-11 14:45:34 -0500

    vite config changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import * as path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: path.join(__dirname, 'src'),
   build:{
     outDir: "build",
     rollupOptions: {
