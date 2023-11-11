bade41a3dd264265c10043616bac2d39eed08856
giggi1226 <gpacheco003@gmail.com>
2023-11-11 15:04:40 -0500

    config fixes

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,6 @@ import * as path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: path.join(__dirname, '/src'),
-  build:{
-    outDir: "build",
-    rollupOptions: {
-      external: 'NonExistingPath'
-    }
-  },
   resolve: {
     alias: {
       "@assets": path.resolve(__dirname, "src/assets"),
